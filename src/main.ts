431ee98052fbb86adbc85826066274e1c75ae0ca
“yakult” <yakult384153@gmail.com>
2026-01-30 16:46:48 +0800

    加入路徑與套件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ViewUIPlus from 'view-ui-plus'
+import tailwindcss from '@tailwindcss/vite'
 
 import App from './App.vue'
 import router from './router'
@@ -7,6 +9,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router).use(ViewUIPlus)
 
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: '/main',
+  },
+  { path: '/main', name: 'main', component: () => import('../view/mainPage.vue') },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
