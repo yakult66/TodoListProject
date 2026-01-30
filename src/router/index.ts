80d737797ce48d1f5b0f4f39d32f3522cf125ac2
“yakult” <yakult384153@gmail.com>
2026-01-30 17:19:21 +0800

    初始化設定並建立mainPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
+const routes = [
+  {
+    path: '/',
+    name: 'mainPage',
+    component: () => import('../App.vue'),
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
