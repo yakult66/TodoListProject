c1e57300711435e1c46a979e37f1a13c99e6eb45
yakult <yakult384153@gmail.com>
2026-02-01 00:38:19 +0800

    加入primeicons套件

--- src/plugins/setup.ts
+++ src/plugins/setup.ts
@@ -1,15 +1,20 @@
 import type { App } from 'vue'
 import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
 import router from '../router';
 import { createPinia } from 'pinia';
+import 'primeicons/primeicons.css';
 
 export function setup(app: App) {
     app.use(createPinia())
     app.use(router)
     app.use(PrimeVue, {
         theme: {
-            preset: Aura
+            options: {
+                cssLayer: {
+                    name: 'primevue',
+                    order: 'theme, base, primevue, utilities'
+                }
+            }
         }
     });
 }
