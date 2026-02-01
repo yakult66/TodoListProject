f705edc5b9463cdc4b60d06976442d0ab9f99815
yakult <yakult384153@gmail.com>
2026-02-02 01:07:22 +0800

    暫存編輯todolist的畫面檔案

---
+++ src/component/taskUpdate.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="flex items-center justify-between gap-2 z-50">
+      <Card>
+        <template #title>
+          {{ taskTitle }}
+        </template>
+        <template #content>
+          <div>
+            <FloatLabel variant="on">
+              <Textarea id="on_label" v-model="taskContent" rows="5" cols="30" style="resize: none" />
+              <label for="on_label">任務內容</label>
+            </FloatLabel>
+          </div>
+        </template>
+      </Card>
+    </div>
+</template>
+
+<script setup lang="ts">
+ import { ref } from 'vue';
+
+ const taskContent = ref('');
+ const taskTitle = ref('');
+</script>
