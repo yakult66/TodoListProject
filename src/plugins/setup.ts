b0b2bdc1c9dde276a7b247c20f0908a05942756a
yakult <yakult384153@gmail.com>
2026-02-01 14:23:27 +0800

    peimevue主題設定

--- src/plugins/setup.ts
+++ src/plugins/setup.ts
@@ -4,12 +4,14 @@ import router from '../router';
 import { createPinia } from 'pinia';
 import 'primeicons/primeicons.css';
 import '../assets/main.css';
+import Aura from '@primeuix/themes/aura';
 
 export function setup(app: App) {
     app.use(createPinia())
     app.use(router)
     app.use(PrimeVue, {
         theme: {
+            preset: Aura,
             options: {
                 cssLayer: {
                     name: 'primevue',
